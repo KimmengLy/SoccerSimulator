09414bb3b2e4e32a66cc137d86c168b82d386d47
Kimmeng LY <kimmeng@outlook.fr>
2017-02-19 10:36:02 +0100

    SoloStrat

--- MesStrategies.py
+++ MesStrategies.py
@@ -5,7 +5,22 @@ from tools import Item, Action
 from soccersimulator.utils import Vector2D
 from soccersimulator.settings import GAME_HEIGHT, GAME_WIDTH
 
-
+class Solo(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Solo")
+    def compute_strategy (self, state, id_team, id_player):
+        tools=Action(state,id_team,id_player)
+        if id_team==1 :
+            if  state.ball.position.x>GAME_WIDTH*3/4.0 :
+                return tools.aller(tools.ball_position)+tools.shoot_but
+            if tools.distance_balle() <= 1.65:
+                return tools.ralentir(tools.ball_position)+tools.shoot_but
+            if state.ball.position.x<(GAME_WIDTH/4.0):
+                return tools.aller(tools.ball_position)+tools.shoot_but
+            if tools.distance_balle()<=1.65:
+                return tools.ralentir(tools.ball_position)+tools.dribble()
+            return tools.aller(tools.ball_position)
+            
 class Fonceur(Strategy):
     def __init__(self):
         Strategy.__init__(self, "Fonceur")
@@ -56,3 +71,4 @@ class Dribbleur(Strategy):
             return tools.aller(tools.ball_position)+tools.dribble
         return SoccerAction(Vector2D(),Vector2D()) 
         
+
