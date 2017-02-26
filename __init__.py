808fbdf83a6009bd3bb1259449346822045f7dda
Kimmeng LY <kimmeng@outlook.fr>
2017-02-26 14:00:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,15 +4,15 @@ from MesStrategies import Solo2, Attaquant, Def2, Attaquant2
 def get_team(i):
     team = SoccerTeam(name="Legendary")
     if (i==1):
-        team.add("El Matador",Solo2()) #Strategie Solo
+        team.add("Eto'o",Solo2()) #Strategie Solo
     if (i==2):
-        team.add("El Matador",Attaquant2()) #Strategie Attaquant
-        team.add("O Monstro",Def2()) #Strategie Defenseur
+        team.add("Messi",Attaquant2()) #Strategie Attaquant
+        team.add("Puyol",Def2()) #Strategie Defenseur
     if (i==4):
-        team.add("El Matador",Attaquant2()) #Strategie Attaquant
-        team.add("O Monstro",Def2()) #Strategie Defenseur
-        team.add("LeMur",Def2())
-        team.add("El Matador",Attaquant2())
+        team.add("Drogba",Attaquant2()) #Strategie Attaquant
+        team.add("Boateng",Def2()) #Strategie Defenseur
+        team.add("Evra",Def2())
+        team.add("Lampard",Attaquant2())
     return team 
             
 
