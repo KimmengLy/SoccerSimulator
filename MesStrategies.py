6d096f1890b7e8c9cfba95276a8fafa172d79e22
Kimmeng LY <kimmeng@outlook.fr>
2017-02-26 12:57:33 +0100

    en test

--- MesStrategies.py
+++ MesStrategies.py
@@ -21,14 +21,35 @@ class Solo(Strategy):
         return tools.aller(tools.ball_position)
 
 
+class Solo2(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Solo")
+    def compute_strategy (self, state, id_team, id_player):
+        tools=Action(state,id_team,id_player)
+        
+        if id_team==1 :
+            if  tools.ball_position.x>(GAME_WIDTH*3/4.0)-10 :
+                if tools.can_shoot :
+                    return tools.tir_angle()
+            if tools.can_shoot :
+                    return tools.dribble()
+            return tools.aller_vect()
+        if tools.ball_position.x<(GAME_WIDTH/4.0)+10:
+            if tools.can_shoot :
+                return tools.tir_angle()
+        if tools.can_shoot :
+            return tools.dribble()
+        return tools.aller_vect()
+        
+        
 class Fonceur(Strategy):
     def __init__(self):
         Strategy.__init__(self, "Fonceur")
     def compute_strategy (self, state, id_team, id_player):
         tools = Action(state,id_team,id_player)
-        if PlayerState.can_shoot :
-            return tools.aller(tools.ball_position)+tools.shoot_but
-        return SoccerAction(Vector2D(),Vector2D()) 
+        if tools.can_shoot :
+            return tools.shoot_but
+        return tools.aller(tools.ball_position)
         
 
 class Attaquant(Strategy):
@@ -60,6 +81,21 @@ class Defenseur(Strategy):
                 return tools.aller(tools.ball_position)+ tools.shoot_but
             return tools.aller(tools.position_defenseur) 
         return SoccerAction(Vector2D(),Vector2D())
+        
+class Def2(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Defenseur")
+    def compute_strategy (self, state, id_team, id_player):
+        tools = Action(state,id_team,id_player)
+        if tools.can_shoot :
+            return tools.shoot_but
+        if id_team == 1:
+            if state.ball.position.x<(GAME_WIDTH/4.0)+10 :
+                return tools.aller_vect()
+        
+        if state.ball.position.x>(GAME_WIDTH*(3.0/4)) :
+            return tools.aller_vect()
+        return tools.aller(tools.position_defenseur) 
 
 
 class Dribbleur(Strategy):
--- Test.py
+++ Test.py
@@ -15,42 +15,21 @@ class Testcoeq(Strategy):
         tool = Item(state,id_team,id_player)
         return tools.aller(tool.equipier_pos()) # tools.shoot_vers(Item(state,id_team,id_player).equipier_pos())
 
-class Fonceur(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Fonceur")
-    def compute_strategy (self, state, id_team, id_player):
-        tools = Action(state,id_team,id_player)
-        return tools.aller(tools.ball_position)+tools.shoot_but
-      
-class Defenseur(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Defenseur")
-    def compute_strategy (self, state, id_team, id_player):
-        tools = Action(state,id_team,id_player)
-        tool = Item(state,id_team,id_player)
-        if id_team == 1:
-            if state.ball.position.x<(GAME_WIDTH/4.0) :
-                #if tool.distance(tool.Vect_anticipe()) <= 0.5:
-                    #return tools.ralentir(tool.Vect_anticipe())+tools.shoot_but
-                return tools.aller(tools.ball_position)+tools.shoot_but#tools.shoot_but#+tools.shoot_vers(Item(state,id_team,id_player).equipier_pos())
-            return tools.aller(tools.position_defenseur)
-        if state.ball.position.x>(GAME_WIDTH*(3.0/4)) :
-            return tools.aller(tool.Vect_anticipe())+tools.shoot_but
-        return tools.aller(tools.position_defenseur) 
-
 
 class DefTest(Strategy):
     def __init__(self):
         Strategy.__init__(self, "Defenseur")
     def compute_strategy (self, state, id_team, id_player):
         tools = Action(state,id_team,id_player)
-        tool = Item(state,id_team,id_player)
+        if tools.can_shoot :
+            return tools.shoot_but
         if id_team == 1:
-            if tools.distance_balle() <= 1.65:
-                return tools.aller_vect_ralenti()+tools.shoot_but
             if state.ball.position.x<(GAME_WIDTH/4.0)+10 :
-                return tools.aller_vect()+tools.shoot_but
-        return tools.aller(tool.position_defenseur)
+                return tools.aller_vect()
+        
+        if state.ball.position.x>(GAME_WIDTH*(3.0/4)) :
+            return tools.aller_vect()
+        return tools.aller(tools.position_defenseur) 
         
 
 class Immobile(Strategy):
@@ -66,10 +45,10 @@ if __name__ == "__main__":
     team2 = SoccerTeam(name="team2")
     #team1.add("El Matador",Test()) #Strategie Fonceur
     #team1.add("Omonstro",Testcoeq()) #Strategie Attaquant
-    team2.add("messi",DefTest()) #Strategie Defenseu
-    
-    team1.add("ujfgj",MesStrategies.Solo()) #Strategie Attaquant
-    #team2.add("messi2",Attaquant())
+    team1.add("messi",MesStrategies.Def2()) #Strategie Defenseu
+    team2.add("ujfgj",MesStrategies.Solo())
+    team1.add("ujfgj",MesStrategies.Solo2()) #Strategie Attaquant
+    team2.add("messi",MesStrategies.Def2())
     #Creation d'une partie
     simu = Simulation(team1, team2)
     #Jouer et afficher la partie
