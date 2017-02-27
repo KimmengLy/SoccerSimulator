1b7bd8214c8ad3c1d1a1d02fc2fb462521e345a2
Kimmeng LY <kimmeng@outlook.fr>
2017-02-27 19:18:38 +0100

    tme7

--- MesStrategies.py
+++ MesStrategies.py
@@ -1,27 +1,10 @@
 from soccersimulator.strategies  import Strategy
-from soccersimulator.mdpsoccer import SoccerTeam, Simulation, SoccerAction, PlayerState
-from tools import Item, Action
+from soccersimulator.mdpsoccer import SoccerAction, PlayerState
 from soccersimulator.utils import Vector2D
-from soccersimulator.settings import GAME_HEIGHT, GAME_WIDTH
+from soccersimulator.settings import GAME_WIDTH
+from tools import Strats, Action
 
 class Solo(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Solo")
-    def compute_strategy (self, state, id_team, id_player):
-        tools=Action(state,id_team,id_player)
-        if id_team==1 :
-            if  tools.ball_position.x>GAME_WIDTH*3/4.0 :
-                return tools.aller(tools.ball_position)+tools.tir_angle()
-            if tools.distance_ball() <= 1.65:
-                return tools.ralentir(tools.ball_position)+tools.dribble()
-        if tools.ball_position.x<(GAME_WIDTH/4.0):
-                return tools.aller(tools.ball_position)+tools.tir_angle()
-        if tools.distance_ball()<=1.65:
-                return tools.ralentir(tools.ball_position)+tools.dribble()
-        return tools.aller(tools.ball_position)
-
-
-class Solo2(Strategy):
     def __init__(self):
         Strategy.__init__(self, "Solo")
     def compute_strategy (self, state, id_team, id_player):
@@ -30,17 +13,59 @@ class Solo2(Strategy):
         if id_team==1 :
             if  tools.ball_position.x>(GAME_WIDTH*3/4.0)-10 :
                 if tools.can_shoot :
-                    return tools.tir_angle()
+                    return tools.shoot_angle
             if tools.can_shoot :
-                    return tools.dribble()
-            return tools.aller_vect()
+                    return tools.dribble
+            return tools.aller_vect
         if tools.ball_position.x<(GAME_WIDTH/4.0)+10:
             if tools.can_shoot :
-                return tools.tir_angle()
+                return tools.shoot_angle
         if tools.can_shoot :
-            return tools.dribble()
-        return tools.aller_vect()
+            return tools.dribble
+        return tools.aller_vect
         
+
+class Attaquant(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Solo")
+    def compute_strategy (self, state, id_team, id_player):
+        tools=Action(state,id_team,id_player)
+        
+        if id_team==1 :
+            if state.ball.position.x>GAME_WIDTH/4.0-10 :
+                if  tools.distance_shoot :
+                    if tools.can_shoot :
+                        return tools.shoot_angle
+                if tools.can_shoot :
+                    return tools.dribble
+                return tools.aller_vect
+            return tools.aller(tools.position_centre)
+        
+        if state.ball.position.x<GAME_WIDTH*(3.0/4)+10 :
+            if tools.distance_shoot:
+                if tools.can_shoot :
+                    return tools.shoot_angle
+            if tools.can_shoot :
+                return tools.dribble
+            return tools.aller_vect
+        return tools.aller(tools.position_centre)
+        
+        
+class Defenceur(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Defenseur")
+    def compute_strategy (self, state, id_team, id_player):
+        tools = Action(state,id_team,id_player)
+        if tools.can_shoot :
+            return tools.passe
+        if id_team == 1:
+            if state.ball.position.x<(GAME_WIDTH/4.0)+10 :
+                return tools.aller_vect
+            return tools.aller(tools.position_defenseur)
+        if state.ball.position.x>(GAME_WIDTH*(3.0/4)) :
+            return tools.aller_vect
+        return tools.aller(tools.position_defenseur) 
+    
         
 class Fonceur(Strategy):
     def __init__(self):
@@ -50,52 +75,6 @@ class Fonceur(Strategy):
         if tools.can_shoot :
             return tools.shoot_but
         return tools.aller(tools.ball_position)
-        
-
-class Attaquant(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Attaquant")
-    def compute_strategy (self, state, id_team, id_player):
-        tools = Action(state,id_team,id_player)
-        if PlayerState.can_shoot :
-            if id_team==1 :
-                if state.ball.position.x>GAME_WIDTH/4.0 :
-                    return tools.aller(tools.ball_position)+tools.shoot_but
-                return tools.aller(tools.position_centre)
-            if state.ball.position.x<GAME_WIDTH*(3.0/4) : 
-                return tools.aller(tools.ball_position)+tools.shoot_but
-        return tools.aller(tools.position_centre)
-  
-      
-class Defenseur(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Defenseur")
-    def compute_strategy (self, state, id_team, id_player):
-        tools = Action(state,id_team,id_player)
-        if PlayerState.can_shoot :
-            if id_team == 1:
-                if state.ball.position.x<(GAME_WIDTH/4.0) :
-                    return tools.aller(tools.ball_position)+ tools.shoot_but
-                return tools.aller(tools.position_defenseur)
-            if state.ball.position.x>(GAME_WIDTH*(3.0/4)) :
-                return tools.aller(tools.ball_position)+ tools.shoot_but
-            return tools.aller(tools.position_defenseur) 
-        return SoccerAction(Vector2D(),Vector2D())
-        
-class Def2(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Defenseur")
-    def compute_strategy (self, state, id_team, id_player):
-        tools = Action(state,id_team,id_player)
-        if tools.can_shoot :
-            return tools.shoot_but
-        if id_team == 1:
-            if state.ball.position.x<(GAME_WIDTH/4.0)+10 :
-                return tools.aller_vect()
-        
-        if state.ball.position.x>(GAME_WIDTH*(3.0/4)) :
-            return tools.aller_vect()
-        return tools.aller(tools.position_defenseur) 
 
 
 class Dribbleur(Strategy):
@@ -107,29 +86,11 @@ class Dribbleur(Strategy):
             return tools.aller(tools.ball_position)+tools.dribble
         return SoccerAction(Vector2D(),Vector2D()) 
         
-class Attaquant2(Strategy):
+class Solo_test(Strategy):
     def __init__(self):
-        Strategy.__init__(self, "Solo")
+        Strategy.__init__(self, "Solo_test")
     def compute_strategy (self, state, id_team, id_player):
-        tools=Action(state,id_team,id_player)
-        
-        if id_team==1 :
-            if state.ball.position.x>GAME_WIDTH/4.0-10 :
-                if  tools.ball_position.x>(GAME_WIDTH*3/4.0) :
-                    if tools.can_shoot :
-                        return tools.tir_angle()
-                if tools.can_shoot :
-                    return tools.dribble()
-                return tools.aller_vect()
-            return tools.aller(tools.position_centre)
-        
-        if state.ball.position.x<GAME_WIDTH*(3.0/4)+10 :
-            if tools.ball_position.x<(GAME_WIDTH/4.0):
-                if tools.can_shoot :
-                    return tools.tir_angle()
-            if tools.can_shoot :
-                return tools.dribble()
-            return tools.aller_vect()
-        return tools.aller(tools.position_centre)
-
-
+        tools=Strats(state,id_team,id_player)
+        if tools.coups_denvoi:
+            return tools.fonce
+        return tools.solo_test
--- tools.py
+++ tools.py
@@ -4,13 +4,24 @@
 from soccersimulator.mdpsoccer import SoccerAction
 from soccersimulator.utils import Vector2D
 from soccersimulator.settings import GAME_HEIGHT, GAME_WIDTH, PLAYER_RADIUS, BALL_RADIUS
-from math import sqrt
 
 class Item(object):
     def __init__(self,state,id_team,id_player):
         self.state = state
         self.key = (id_team,id_player)
     
+    @property
+    def team_1(self):
+        if self.key[0]==1:
+            return True
+        return False
+        
+    @property
+    def team_2(self):
+        if self.key[0]==2:
+            return True
+        return False
+        
     @property
     def ball_position(self):
         """renvoi les coordonées x et y de la balle sous la forme d'un couple (x,y)"""
@@ -21,10 +32,10 @@ class Item(object):
         """renvoi les coordonées x et y de la balle sous la forme d'un couple (x,y)"""
         return Vector2D(self.ball_position.x+self.ball_position.norm,self.ball_position.y+self.ball_position.norm)
     
+    @property
     def my_position(self):
         """renvoi la position d'un joueur sous la forme d'un couple (x,y)"""
         return self.state.player_state(self.key[0],self.key[1]).position
-        #equivalent a self.player_state(self.key[0],self.key[1])
         
     @property
     def position_centre(self):
@@ -40,62 +51,64 @@ class Item(object):
             return Vector2D(GAME_WIDTH*(15.0/16),GAME_HEIGHT/2)
         return Vector2D(GAME_WIDTH/16.0,GAME_HEIGHT/2)
     
-        
-    def equipier_pos(self):
-        """renvoi la position de l'équipier le plus proche"""
-        #float d_min
-        #d_min= GAME_WIDTH
-        #(id_team,id player) joueur proche   
-        
-        liste= self.state.players
-        for (i,j) in liste:
-            """players renvoi la liste des clés (id_team,id_player)"""
-            #(id_team,id_player) joueur
-            
-            
-            if((self.key[0]==i) and (self.key[1]!=j)): # and (self.distance_joueur(i,j)<d_min)):
-                #d_min=self.distance_joueur(i,j)
-                self.key= (i, j)
-        return self.my_position()
+    @property    
+    def equipier(self):
+        """renvoi la position de l'équipier le plus proche""" 
+        for (i,j) in self.state.players:
+            if((self.key[0]==i) and (self.key[1]!=j)):
+                return self.state.player_state(i, j)
     
+    @property    
+    def equipier_pos(self):
+        """renvoi la position de l'équipier le plus proche""" 
+        for (i,j) in self.state.players:
+            if((self.key[0]==i) and (self.key[1]!=j)):
+                return self.state.player_state(i, j).position
+                
+    @property 
     def distance_ball(self):
         """renvoi la distance  entre un joueur et la balle"""
-        return (self.ball_position-self.my_position()).norm
-        
-    def distance(self, p):
-        """renvoi la distance  entre 2 joueurs sous la forme d'un nombre flottant"""
-        xa=self.my_position().x
-        xb=p.x
-        ya=self.my_position().y
-        yb=p.y
-        return (sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)))
-        
-    def distance_joueur(self, i, j):
-        return self.my_position().distance(self.state.player_state(i, j))
-
+        return (self.ball_position-self.my_position).norm
     
-       
+    @property    
+    def distance_player(self, p):
+        """renvoi la distance  entre 2 joueurs sous la forme d'un nombre flottant"""
+        return (p-self.my_position).norm
+    
+    @property              
     def vect_anticipe(self):    
         return self.state.ball.position+ 5*self.state.ball.vitesse
     
-
+    @property              
+    def vect_anticipe_eq(self):    
+        return self.equipier.position+ 51*self.equipier.vitesse
+    
+    @property
     def position_cage(self):
         if self.key[0]==2:
             return Vector2D(0,GAME_HEIGHT/2)
         return Vector2D(GAME_WIDTH,GAME_HEIGHT/2)
-        
-    def scalaire(self, p):
-        xa=self.my_position().x
-        xb=p.x
-        ya=self.my_position().y
-        yb=p.y
-        return (xa*xb)+(yb*ya)
-   
+
     @property
     def can_shoot( self ) :
-        if (self.ball_position-self.my_position()).norm>= PLAYER_RADIUS + BALL_RADIUS:
+        if (self.ball_position-self.my_position).norm>= PLAYER_RADIUS + BALL_RADIUS:
                 return False
         return True
+    
+    @property
+    def distance_shoot(self):
+        if (self.position_cage-self.my_position).norm<=45:
+            return True
+        return False
+    
+    @property
+    def coups_denvoi(self):
+        if self.ball_position.x==GAME_WIDTH/2: 
+            if self.ball_position.y==GAME_HEIGHT/2:
+                return True
+        return False
+    
+
      
 class Action(Item): 
     @property
@@ -103,47 +116,84 @@ class Action(Item):
         if self.key[0] == 2 :
             return SoccerAction(Vector2D(),Vector2D(0, GAME_HEIGHT/2)-self.state.ball.position)
         return SoccerAction(Vector2D(),Vector2D(GAME_WIDTH, GAME_HEIGHT/2)-self.state.ball.position)
-  
+    
     def aller(self,p):
-        return SoccerAction(p-self.my_position(),Vector2D())
+        return SoccerAction(p-self.my_position,Vector2D())
         
-    def ralentir(self, p):
-        return SoccerAction(p/250.0 -self.my_position()/250.0,Vector2D())
-        
+    def ralentir_p(self, p):
+        return SoccerAction(p/250.0 -self.my_position/250.0,Vector2D())
+   
+    @property    
     def passe(self):
         return SoccerAction(Vector2D(),self.equipier_pos-self.ball_position)
     
-    def shoot_vers(self, p):
+    def shoot_to(self, p):
         return SoccerAction(Vector2D(), p-self.ball_position)
     
+    @property
     def immobile(self):
         return SoccerAction(Vector2D(), Vector2D())
-        
+    
+    @property
     def tout_droit(self):
         if self.key[0]==2 :
             return SoccerAction(Vector2D(-2,0),Vector2D())
         return SoccerAction(Vector2D(2,0),Vector2D())
-        
-    def dribble(self):
-        vec_pos = self.position_cage() - self.ball_position
-        return SoccerAction(Vector2D(),Vector2D( angle = vec_pos.angle, norm = vec_pos.norm/60 ))
-       
-    def tir_angle(self):
-        vec_pos = self.position_cage() - self.ball_position
-        return SoccerAction(Vector2D(),Vector2D( angle = vec_pos.angle, norm = vec_pos.norm/6.5 ))
-         
-   # def can_shoot(self):
-    def test_aller(self, p):
-        if (self.distance_ball()<=1.65):
-            return SoccerAction(p/50-self.my_position()/50,Vector2D())   
-        return SoccerAction(p-self.my_position(),Vector2D())
-
-    def aller_vect(self):
-        return SoccerAction(self.vect_anticipe()-self.my_position(),Vector2D())
-        
     
+    @property
+    def dribble(self):
+        vec_pos = self.position_cage - self.ball_position
+        return SoccerAction(Vector2D(),Vector2D( angle = vec_pos.angle, norm = vec_pos.norm/50 ))
+    
+    @property
+    def passe_angle(self):
+        vec_pos = self.equipier_pos - self.ball_position
+        return SoccerAction(Vector2D(),Vector2D( angle = vec_pos.angle, norm = vec_pos.norm/8 ))
+     
+    @property 
+    def shoot_angle(self):
+        vec_pos = self.position_cage - self.ball_position
+        return SoccerAction(Vector2D(),Vector2D( angle = vec_pos.angle, norm = vec_pos.norm/8 ))
+    @property     
+    def aller_coeq(self):
+        return self.aller(self.equipier_pos)
+
+    @property
+    def aller_vect(self):
+        return SoccerAction(self.vect_anticipe-self.my_position,Vector2D())
+        
+    @property
     def aller_vect_ralenti(self):
-        return SoccerAction((self.vect_anticipe()-self.my_position())/800,Vector2D())
+        return SoccerAction((self.vect_anticipe-self.my_position)/800,Vector2D())
+    
+    @property
+    def degagement(self):
+        return SoccerAction(Vector2D(),self.position_centre-self.ball_position)
+
+class Strats(Action):
+    
+    @property
+    def solo_test(self):
+        if self.team_1 :
+            if self.distance_shoot :
+                if self.can_shoot :
+                    return self.shoot_angle
+            if self.can_shoot :
+                    return self.dribble
+            return self.aller_vect
+        if  self.distance_shoot:
+            if self.can_shoot :
+                return self.shoot_angle
+        if self.can_shoot :
+            return self.dribble
+        return self.aller_vect
+        
+    @property
+    def fonce(self):
+        if self.can_shoot :
+            return self.shoot_but
+        return self.aller(self.ball_position)
+    
 
         
 
